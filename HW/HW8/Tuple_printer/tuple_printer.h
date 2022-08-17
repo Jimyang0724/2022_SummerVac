3ec7e87d9067df70a3973681c1cd76a52c91c617
jimyang <jimyang890724@gmail.com>
2022-08-17 13:24:50 +0800

    [UPDATE] add tuple_printer

--- HW/HW8/Tuple_printer/tuple_printer.h
+++ HW/HW8/Tuple_printer/tuple_printer.h
@@ -1,2 +1,28 @@
 #pragma once
+#include <iostream>
+#include <tuple>
 
+using namespace std;
+
+namespace tuple_printer {
+    template<typename Tuple, size_t ...Is>
+    auto _pop_front(Tuple& t, index_sequence<Is...>) {
+        return make_tuple(get<1 + Is>(t)...);
+    }
+
+    template<typename T>
+    auto pop_front(T& t) {
+        return _pop_front(t, make_index_sequence<tuple_size<T>::value -1 >());
+    }
+
+    template<typename T>
+    auto printer(T t) -> void {
+        cout << endl;
+    }
+
+    template<typename T, typename... Ts>
+    auto printer(tuple<T, Ts...> t) -> void {
+        cout << get<0>(t)<< " ";
+        printer(pop_front(t));
+    }
+}
--- HW/HW8/main.cpp
+++ HW/HW8/main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include <tuple>
-
+#include "tuple_printer.h"
 using namespace std;
 
 int main() {
     tuple<int, int, int> t1(1, 2, 3);
+    tuple_printer::printer(t1);
     return 0;
 }
